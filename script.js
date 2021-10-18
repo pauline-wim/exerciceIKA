ebaecf286a87907e479ecc15dd0a5896e75d0a4e
pauline-wim <linette.wim@hotmail.com>
2021-10-18 14:31:06 +0200

    randomNumbers-noRepeat

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Lier l'input mot de passe au keypad
 function input(e) {
     var password = document.getElementById("password");
     password.value = password.value + e.value;
@@ -5,22 +6,31 @@ function input(e) {
 
 
 // Random value - KEYPAD 
-for (i = 0; i < 10; i++) {
-    var btn = document.getElementById("btn" + i);
-    btn.value = Math.floor(Math.random() * 10);
+// for (i = 0; i < 10; i++) {
+//     var btn = document.getElementById("btn" + i);
+//     btn.value = Math.floor(Math.random() * 10);
+// }
+
+var btn = document.getElementById("btn" + i);
+var nums = [0,1,2,3,4,5,6,7,8,9];
+var gen_nums = [];
+
+function in_array(array, el) {
+   for(var i = 0 ; i < array.length; i++) 
+       if(array[i] == el) return true;
+   return false;
 }
 
-// function load() {
-//     var array = new Array();
+function get_rand(array) {
+    var rand = array[Math.floor(Math.random()*array.length)];
+    if(!in_array(gen_nums, rand)) {
+       gen_nums.push(rand); 
+       return rand;
+    }
+    return get_rand(array);
+}
 
-//     while (array.length < 10) {
-//         var temp = Math.round(Math.random() * 9);
-//         if (!contain(array, temp)) {
-//             array.push(temp);
-//         }
-//     }
-//     for (i = 0; i < 10; i++) {
-//         var btn = document.getElementById("btn" + i);
-//         btn.value = array[i];
-//     }
-// }
+for(var i = 0; i < 10; i++) {
+    var btn = document.getElementById("btn" + i);
+    btn.value = get_rand(nums);
+}
